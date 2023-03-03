1d256961c8916275601fcef2759579c4137c51e3
jucian0 <juciano@outlook.com.br>
2023-03-03 07:55:07 -0300

    fix: adding function to resolve changelog

--- packages/semver/src/GenerateChangelog.ts
+++ packages/semver/src/GenerateChangelog.ts
@@ -2,8 +2,9 @@ import conventionalChangelog from "conventional-changelog";
 import { cwd } from "process";
 import { getCommits } from "./GitCommands";
 import * as fs from "fs";
-import { Preset } from "../semver.schema";
+
 import { Config } from "./Types";
+import { WriteChangelogConfig } from "../semver.schema";
 
 export async function generateChangelog(
   config: Config,
@@ -12,33 +13,55 @@ export async function generateChangelog(
 ) {
   const context = { version: nextVersion };
 
-  const outputStream = fs.createWriteStream(`${pkgPath}/CHANGELOG.md`, {
-    flags: "w+",
-  });
+  let changelog = "";
 
   conventionalChangelog(
     {
       preset: config.preset,
       tagPrefix: config.tagPrefix,
-      config: {
-        writerOpts: {
-          headerPartial: "#changelog",
-        },
-      },
     },
     context,
     {
       merges: null,
-      path: `${pkgPath}/package.json`,
+      path: pkgPath,
     } as conventionalChangelog.Options
   )
-    .pipe(outputStream)
-    .on("close", () => {
-      console.log("Success");
+    .on("error", function (err) {
+      //  reject(err);
     })
-    .on("error", (err) => {
-      console.log(err);
+
+    .on("data", function (buffer: ArrayBuffer) {
+      changelog += buffer.toString();
+    })
+
+    .on("end", function () {
+      fs.writeFileSync(
+        `${pkgPath}/CHANGELOG.md`,
+        `# Changelog` +
+          "\n" +
+          (changelog + buildExistingContent(`${pkgPath}/CHANGELOG.md`)).replace(
+            /\n+$/,
+            "\n"
+          ),
+        "utf8"
+      );
     });
+}
+
+const START_OF_LAST_RELEASE_PATTERN =
+  /(^#+ \[?[0-9]+\.[0-9]+\.[0-9]+|<a name=)/m;
+
+function buildExistingContent(changelogPath: string) {
+  const existingContent = fs.readFileSync(changelogPath, "utf-8");
+  const existingContentStart = existingContent.search(
+    START_OF_LAST_RELEASE_PATTERN
+  );
+  // find the position of the last release and remove header:
+  if (existingContentStart !== -1) {
+    return existingContent.substring(existingContentStart);
+  }
+
+  return existingContent;
 }
 
 export function generateAllChangelogs(
