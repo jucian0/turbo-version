56f6e15ab6078ec909f1dfa70b27d1b9a204c4f1
jucian0 <juciano@outlook.com.br>
2023-03-13 23:41:31 -0300

    test: adding tests generateVersion

---
+++ packages/semver/tests/utils/GetLatestTag.test.ts
@@ -0,0 +1,40 @@
+import {describe, expect, test,jest, afterEach} from '@jest/globals';
+import gitSemverTags from 'git-semver-tags';
+import { getLatestTag } from '../../src/utils/GetLatestTag';
+
+jest.mock('git-semver-tags');
+
+describe('getLatestTag', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('should return the latest tag with the specified prefix', async () => {
+    const versions = ['v1.2.0', 'v1.0.1', 'v1.0.0'];
+    (gitSemverTags as any).mockImplementationOnce((options: any, cb: any) => {
+      cb(null, versions);
+    });
+
+    const latestTag = await getLatestTag('v');
+    console.log(latestTag)
+    expect(latestTag).toBe('v1.2.0');
+  });
+
+  test('should return an empty string if no tags are found', async () => {
+    (gitSemverTags as any).mockImplementationOnce((options: any, cb: any) => {
+      cb(null, []);
+    });
+
+    const latestTag = await getLatestTag('v1');
+    expect(latestTag).toBe('');
+  });
+
+  test('should handle errors gracefully', async () => {
+    const error = new Error('Failed to get tags');
+    (gitSemverTags as any).mockImplementationOnce((options: any, cb: any) => {
+      cb(error);
+    });
+
+    await expect(getLatestTag('v1')).rejects.toThrow(error);
+  });
+});
