a30748ca42c21b391f52bf8f7240380e05aa4079
Jucian0 <juciano@outlook.com.br>
2024-11-14 20:34:02 -0300

    end

--- rust-cli/src/strategies/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod affected_packages_strategy;
-//pub mod all_packages_strategy;
--- rust-cli/src/utils/mod.rs
+++
@@ -1,9 +0,0 @@
-// pub use git::get_git_commit_messages;
-// pub use git::get_git_commits;
-// pub use git::get_git_commits_length_since_last_tag;
-// pub use git::get_last_tag;
-
-// pub use internal_version::get_internal_version;
-pub mod get_packages;
-
-pub mod git;
