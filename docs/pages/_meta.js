bd5f8ef2ccb3139e6c5c08693fda125888b0d40d
Jucian0 <juciano@outlook.com.br>
2024-12-25 22:03:25 -0300

    docs(docs): adding docs

---
+++ docs/pages/_meta.js
@@ -0,0 +1,12 @@
+export default{
+  "index": "Introduction",
+  "synced": "Synced",
+  "async": "Async",
+  "versioning": "Versioning",
+  "contact": {
+    "title": "Contact ↗",
+    "type": "page",
+    "href": "https://twitter.com/juciano_barbosa",
+    "newWindow": true
+  }
+}
---
+++ docs/theme.config.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import type { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    content: 'TurboVersion © 2024',
+  },
+}
+
+export default config
+
+
